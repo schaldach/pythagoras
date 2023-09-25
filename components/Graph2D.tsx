a6eabe48e92d3e6ca4691352e7a020630becbddd
schaldach <gabimorgado0311@gmail.com>
2023-09-25 20:45:36 -0300

    Início gráfico interativo

--- components/Graph2D.tsx
+++ components/Graph2D.tsx
@@ -1,47 +1,78 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { evaluate } from "mathjs";
 
 // Width e Height de 500px
 // Visualização padrão no intervalo [-10,10]
-const defaultZoom = 0.04
+const defaultZoom = 0.04;
 
-function expression(x: number) {
-  return Math.sin(x);
+function validateExpression(exp: string, x: number) {
+  const changedExp = exp.replace("x", String(x));
+  try {
+    const result = evaluate(changedExp);
+    return result;
+  } catch (e) {
+    return undefined;
+  }
+}
+interface GraphProps {
+  exp: string;
 }
 
-function Graph2D() {
+function Graph2D({ exp }: GraphProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvasCtx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    const canvas: HTMLCanvasElement = canvasRef.current!;
-    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+    const canvas = canvasRef.current;
+    setCanvas(canvas);
+    if (!canvas) {return;}
+    const ctx = canvas.getContext("2d");
+    setCtx(ctx);
+  }, []);
 
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(0, -canvas.height / 2);
-    ctx.stroke();
+  useEffect(() => {
+    if (!canvasCtx || !canvas) {
+      return;
+    }
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasCtx.translate(canvas.width / 2, canvas.height / 2);
+    canvasCtx.strokeStyle = "#000000";
+    canvasCtx.lineWidth = 1;
 
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(-canvas.width / 2, 0);
-    ctx.stroke();
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(0, canvas.height / 2);
+    canvasCtx.lineTo(0, -canvas.height / 2);
+    canvasCtx.stroke();
 
-    ctx.strokeStyle = "#4C98FA";
-    ctx.lineWidth = 2
-    ctx.beginPath();
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(canvas.width / 2, 0);
+    canvasCtx.lineTo(-canvas.width / 2, 0);
+    canvasCtx.stroke();
+
+    canvasCtx.strokeStyle = "#4C98FA";
+    canvasCtx.lineWidth = 2;
+    canvasCtx.beginPath();
     for (let i = 0; i < canvas.width; i++) {
-      const x = (i - canvas.width / 2)*defaultZoom;
-      const y = expression(x);
+      const x: number = (i - canvas.width / 2) * defaultZoom;
+      const y: number | undefined = validateExpression(exp, x);
+      if (y === undefined) {
+        continue;
+      }
 
-      const canvasX = x/defaultZoom;
-      const canvasY = -y/defaultZoom;
+      const canvasX = x / defaultZoom;
+      const canvasY = -y / defaultZoom;
       if (i === 0) {
-        ctx.moveTo(canvasX, canvasY);
+        canvasCtx.moveTo(canvasX, canvasY);
       } else {
-        ctx.lineTo(canvasX, canvasY);
+        canvasCtx.lineTo(canvasX, canvasY);
       }
     }
-    ctx.stroke();
-  }, []);
+    canvasCtx.stroke();
+    canvasCtx.translate(-canvas.width / 2, -canvas.height / 2);
+
+  }, [exp]);
 
   return <canvas width={500} height={500} ref={canvasRef} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Graph2D from "../../components/Graph2D";
+import { useState, useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const [mathstr, setmathstr] = useState("");
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => setmathstr(query), 500);
+    return () => clearTimeout(timeOutId);
+  }, [query]);
   return (
     <main className={inter.className}>
       <div className="font-semibold flex gap-2 items-center justify-center text-3xl tracking-tight">
@@ -15,8 +24,9 @@ export default function Home() {
         />
         Pythagoras
       </div>
+      <input onChange={e => setQuery(e.target.value)}></input>
       <div className="flex justify-center mt-10">
-        <Graph2D />
+        <Graph2D exp={mathstr}/>
       </div>
     </main>
   );
